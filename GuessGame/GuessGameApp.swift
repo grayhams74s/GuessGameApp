5ae3db23d11222ba2f635e0439d8cd6621bdcc5c
grayhams74s <felixjunang@gmail.com>
2026-01-28 14:18:39 +0800

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by felix angcot jr on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
